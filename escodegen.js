25743be7275361df8eafcea0942f6540dbad52ae
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2015-09-04 20:48:56 -0700

    Fix for supporting AssignmentPattern. Closes #229.
    Reviewed by @tolmasky.

--- escodegen.js
+++ escodegen.js
@@ -2197,6 +2197,10 @@
             return result;
         },
 
+        AssignmentPattern: function(expr, precedence, flags) {
+            return this.generateAssignment(expr.left, expr.right, expr.operator, precedence, flags);
+        },
+
         ObjectPattern: function (expr, precedence, flags) {
             var result, i, iz, multiline, property, that = this;
             if (!expr.properties.length) {
