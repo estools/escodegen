c89ea2f0a524fff89f390241ac2433d5eea0a0d4
sanex3339 <yarabotayuvyandex3339>
2020-06-22 23:00:00 +0300

    Added test to check computed key parenthesis

--- test/compare-acorn-es6/class-declaration.expected.js
+++ test/compare-acorn-es6/class-declaration.expected.js
@@ -2,6 +2,10 @@ class ComputedKey {
     [n1 + n2]() {
     }
 }
+class ComputedKeyWithParenthesis {
+    [(n1 + n2) * n3]() {
+    }
+}
 class SequenceExpressionAsKey {
     [(n1, n2)]() {
     }
--- test/compare-acorn-es6/class-declaration.js
+++ test/compare-acorn-es6/class-declaration.js
@@ -1,6 +1,9 @@
 class ComputedKey {
     [n1 + n2]() {}
 }
+class ComputedKeyWithParenthesis {
+    [(n1 + n2) * n3]() {}
+}
 class SequenceExpressionAsKey {
     [(n1, n2)]() {}
 }
