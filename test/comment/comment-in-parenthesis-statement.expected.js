8c316faf307c693faa111dd33e07202bd32bb729
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-10-07 15:01:15 +0200

    add a test

---
+++ test/comment/comment-in-parenthesis-statement.expected.js
@@ -0,0 +1,5 @@
+function foo(a, b, c) {
+    // lorem
+    // ipsum
+    return a >= b && a <= c || a === 42 || a === 666;
+}
---
+++ test/comment/comment-in-parenthesis-statement.js
@@ -0,0 +1,9 @@
+function foo(a, b, c) {
+    return (
+        (a >= b && a <= c)
+
+        // lorem
+        // ipsum
+        || a === 42 || a === 666
+    );
+}
