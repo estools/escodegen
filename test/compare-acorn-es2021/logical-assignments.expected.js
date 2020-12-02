7e74eb8050cba16e1158b63663d742de6a39633e
Yusuke Suzuki <ysuzuki@apple.com>
2020-12-01 19:19:03 -0800

    Test logical assignments
    escodegen can transparently support logical assignments without any code.
    But we should test it by using acorn with 2021 option.
    This patch adds logical-assignments test cases.

---
+++ test/compare-acorn-es2021/logical-assignments.expected.js
@@ -0,0 +1,12 @@
+undefined &&= 42;
+x &&= ++counter;
+x.a &&= 42;
+x['a'] &&= 42;
+undefined ||= 42;
+x ||= ++counter;
+x.a ||= 42;
+x['a'] ||= 42;
+undefined ??= 42;
+x ??= ++counter;
+x.a ??= 42;
+x['a'] ??= 42;
